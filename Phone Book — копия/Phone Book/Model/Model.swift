d2323acac1f387d11a4c0bfbcc982e8fb13f9fcc
flowereDnap <nikitashapko5@gmail.com>
2021-11-23 19:11:41 +0200

    UDsave+few buggs fixed

--- Phone Book — копия/Phone Book/Model/Model.swift
+++ Phone Book — копия/Phone Book/Model/Model.swift
@@ -7,12 +7,39 @@
 
 import Foundation
 
-public struct Contact{
+public struct Contact: Codable{
     public var name: String
     public var number: String
 }
 
 class Model{
-    static public var data:[Contact] = [Contact(name:"lala",number: " h")]
+    private static var loaded_data:[Contact]? = nil
+    static let key = "contacts_list"
+    static public var data:[Contact]{
+        get{
+            if let loaded_data = loaded_data{
+                return loaded_data
+            }
+            else{
+                if let mydata = UserDefaults.standard.value(forKey:key) as? Data {
+                    let data2:[Contact]? = try? PropertyListDecoder().decode(Array<Contact>.self, from: mydata)
+                    loaded_data = data2 ?? []
+                }
+                
+                return loaded_data!
+            }
+        }
+        set{
+            
+            
+            loaded_data = newValue
+            UserDefaults.standard.set(try? PropertyListEncoder().encode(newValue), forKey:key)
+           
+        
+        }
+    }
     public static var currentContactId:Int = 0
+    
+    
 }
+
